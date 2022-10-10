e574cda2ef6c28c88d8518027fe6c4dc0d9cbbcd
Katharine Armstrong <katharine.armstrong@mail.utoronto.ca>
2022-10-10 19:49:54 -0400

    actually draw when don't hit shadow, use 0 distance for shadow ray hit minimum, normalize ray vector, correct directionality of halfvector

--- src/blinn_phong_shading.cpp
+++ src/blinn_phong_shading.cpp
@@ -7,7 +7,7 @@
 
 // gobal definitions
 double AMBIENT_FACTOR = 0.1;
-double EPSILON_FACTOR = 0.1;
+double EPSILON_FACTOR = 0.0001;
 
 
 /*
@@ -50,40 +50,54 @@ Eigen::Vector3d blinn_phong_shading(
     // object at hit_id is the one we need to compute the colour for
     // Object & obj = objects[hit_id];
     Ray shadow_ray;
-    Eigen::Vector3d rgb, dir_light, shadow_obj_normal, light_diffused, light_specular;
+    Eigen::Vector3d rgb, dir_light, shadow_obj_normal, rgb_diffused, rgb_specular, rgb_ambient, rgb_phong, hit_normal, halfvect;
     double max_t, shadow_hit_t;
     int shadow_hit_id;
     bool hit_obj;
     Eigen::Vector3d object_hit_location = ray.origin + t*ray.direction;
     int num_of_lights = lights.size();
 
-    // get the ambient colour (pre any actual light sources)
-    rgb = AMBIENT_FACTOR*(objects[hit_id]->material->ka);
+    shadow_ray.origin = object_hit_location + EPSILON_FACTOR*n;
 
+    // get the ambient colour (pre any actual light sources)
+    rgb_ambient = AMBIENT_FACTOR*(objects[hit_id]->material->ka);
+    rgb_phong = Eigen::Vector3d::Zero();
     // calculate the colour impact for each light source
     for(int i = 0; i < num_of_lights; ++i){
+        // get the direction of the light and the distance
         // find the direction and distance of the light
         lights[i]->direction(object_hit_location, dir_light, max_t);
-        // check if shadow ray hits anything, if not then contribute light to colour
         shadow_ray.direction = dir_light;
-        shadow_ray.origin = object_hit_location + EPSILON_FACTOR*n;
+
+        // add check for if shadow ray hits here, for now just fix diffuse & Specular
+        rgb_diffused = Eigen::Vector3d::Zero();
+        rgb_specular = Eigen::Vector3d::Zero();
+
+        hit_obj = first_hit(shadow_ray, 0.0, objects, shadow_hit_id, shadow_hit_t, shadow_obj_normal);
         if(
             (max_t == std::numeric_limits<double>::infinity()) ||
-            (!first_hit(shadow_ray, 0, objects, shadow_hit_id, shadow_hit_t, shadow_obj_normal)) ||
-            (shadow_hit_t > max_t)
+            !hit_obj ||
+            (hit_obj && (shadow_hit_t > max_t))
         ) {
-            // directional light, no hit, or obj is farther than max_t
-            // therefore add light from this source based on blinn_phong_shading
+
+            // get diffused light
             double n_dot_l = n.dot(dir_light);
-            light_diffused = (objects[hit_id]->material->kd.array() * lights[i]->I.array()).matrix() * std::max(0.0, n_dot_l);
-            double n_dot_h = n.dot((ray.direction + dir_light).normalized());
-            light_specular = (objects[hit_id]->material->ks.array() * lights[i]->I.array()).matrix() *
-                                std::pow(std::max(0.0, n_dot_h), objects[hit_id]->material->phong_exponent);
-            rgb += light_diffused + light_specular;
+            double max_angle_factor = std::max(0.0, n_dot_l);
+            rgb_diffused = ((objects[hit_id]->material->kd).array() * (lights[i]->I).array()).matrix() * max_angle_factor;
+
+            // get specular light
+            halfvect = (dir_light - ray.direction.normalized()).normalized();
+            double n_dot_h = n.dot(halfvect);
+            double max_angle_phong = std::pow(std::max(0.0, n_dot_h), objects[hit_id]->material->phong_exponent);
+            rgb_specular = ((objects[hit_id]->material->ks).array() * (lights[i]->I).array()).matrix() * max_angle_phong;
+
         }
+
+        rgb_phong += rgb_diffused + rgb_specular;
 
     }
 
+    rgb = rgb_ambient + rgb_phong;
     return rgb;
   ////////////////////////////////////////////////////////////////////////////
 }
