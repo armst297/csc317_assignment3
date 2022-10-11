1e12635d1152f1a9606731c14422e4571adcda77
Katharine Armstrong <katharine.armstrong@mail.utoronto.ca>
2022-10-11 00:00:50 -0400

    working reflection, needed to flip the reflect vector

--- src/raycolor.cpp
+++ src/raycolor.cpp
@@ -3,8 +3,8 @@
 #include "blinn_phong_shading.h"
 #include "reflect.h"
 
-int MAX_RECURSIONS = 0;
-double EPSILON_FACTOR_REFLECT = 0.1;
+int MAX_RECURSIONS = 5;
+double EPSILON_FACTOR_REFLECT = 0.0001;
 
 
 /*
@@ -37,31 +37,34 @@ bool raycolor(
     ////////////////////////////////////////////////////////////////////////////
     // Replace with your code here:
 
-
-
     rgb = Eigen::Vector3d(0,0,0);
-    Eigen::Vector3d rgb_phong, light_mirrored, rgb_mirrored;
+    Eigen::Vector3d rgb_phong, light_mirrored, rgb_mirrored, dir_light;
     Ray reflected_ray;
+    int num_of_lights = lights.size();
 
     // variables needed for first_hit function
     Eigen::Vector3d n = Eigen::Vector3d::Zero();
     int hit_id = -1;
     double t = -1.0;
-
+    double max_t = 0;
 
     if( first_hit(ray, min_t, objects, hit_id, t, n) ){
         // get colour based on blinn_phong_shading
         rgb_phong = blinn_phong_shading(ray, hit_id, t, n, objects, lights);
+
         // add mirrored component IFF applicable
         light_mirrored = Eigen::Vector3d::Zero();
+        rgb_mirrored = Eigen::Vector3d::Zero();
+        Eigen::Vector3d object_hit_location = ray.origin + t*ray.direction;
         if(! (objects[hit_id]->material->km == Eigen::Vector3d::Zero()) ){
             // has a reflective component, calculate the reflection
             if(num_recursive_calls < MAX_RECURSIONS){
                 // need to figure out what colour to return
-                reflected_ray.direction = reflect(ray.direction, n);
+                reflected_ray.direction = reflect(ray.direction.normalized(), n);
                 reflected_ray.origin = (ray.origin + t*ray.direction) + EPSILON_FACTOR_REFLECT*n;
-                raycolor(reflected_ray, 1.0, objects, lights, num_recursive_calls + 1, light_mirrored);
-                rgb_mirrored = (objects[hit_id]->material->km.array() * light_mirrored.array()).matrix();
+                if(raycolor(reflected_ray, 0.0, objects, lights, num_recursive_calls + 1, light_mirrored)){
+                    rgb_mirrored = (objects[hit_id]->material->km.array() * light_mirrored.array()).matrix();
+                }
             }
 
         }
--- src/reflect.cpp
+++ src/reflect.cpp
@@ -4,7 +4,8 @@ Eigen::Vector3d reflect(const Eigen::Vector3d & in, const Eigen::Vector3d & n)
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
-    Eigen::Vector3d reflected_ray = (2*(n.dot(in))*n - in).normalized();
+    // Eigen::Vector3d reflected_ray = (2*(n.dot(in))*n - in).normalized();
+    Eigen::Vector3d reflected_ray = in - 2*(in.dot(n))*n;
     return reflected_ray;
   ////////////////////////////////////////////////////////////////////////////
 }
